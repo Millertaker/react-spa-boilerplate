ecf0c663c3b590221ea67698a0a03c6bd460648b
miller.gonzalez <miller.gonzalez@salesforce.com>
2017-07-04 17:15:35 -0500

    Implement Bootstrap CSS

--- gulp/development.js
+++ gulp/development.js
@@ -10,10 +10,19 @@ var rimraf = require('rimraf');
 var webserver = require('gulp-webserver');
 var run = require('run-sequence');
 
+var uglifycss = require('gulp-uglifycss');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+
 var webpackConfigDev = require('../webpack.config.js');
 
 gulp.task('clean', function(cb){
   rimraf('./public/js', cb);
+});
+
+gulp.task('clean-css', function(cb){
+  rimraf('./public/css', cb);
 });
 
 gulp.task('traspile-dev-scripts', function() {
@@ -22,6 +31,14 @@ gulp.task('traspile-dev-scripts', function() {
 
 gulp.task('watch-fe', function(){
   gulp.watch('./src/**/*.js', ['clean','traspile-dev-scripts']);
+});
+
+gulp.task('less', function(){
+  gulp.src('./less/**/*.less')
+    .pipe(plumber())
+    .pipe(concat('main.css'))
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'))
 });
 
 gulp.task('webserver', function() {
@@ -34,5 +51,5 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('development', function(cb){
-  run('clean', 'watch-fe', 'traspile-dev-scripts', 'webserver', cb);
+  run('clean', 'clean-css', 'less', 'watch-fe', 'traspile-dev-scripts', 'webserver', cb);
 });
